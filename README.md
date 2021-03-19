8f3db7a802be6251f92253d68c9aeebc8dc27857
aleksandrsw <78419066+aleksandrsw@users.noreply.github.com>
2021-03-19 21:07:41 +0200

    Reverted to version 0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Projekts programmēšanā
-Versija 0.2.1
+Versija 0.2
 
 Programmatūra, kas pēc lietotāja vajadzībām atrod izdevīgākās un pieejamākās vietas, kur apmainīt valūtu no EUR uz USD.
--- projekta_programma.py
+++ projekta_programma.py
@@ -1,9 +1,10 @@
 import csv
 import operator
 
-class Error(Exception):
+class Kluda(Exception):
    pass
 
+<<<<<<< HEAD
 def getAnswer(x,input_string):
    """Pieprasa mainīgajam x vērtību 1 vai 2, parādot tekstu input_string. Ja ievadīta nepareiza vērtība, tad tiek paradīta kļūdas ziņa."""
    while True:
@@ -17,6 +18,8 @@ def getAnswer(x,input_string):
       except:
          print("Lūdzu ievadiet 1 vai 2.\n")
 
+=======
+>>>>>>> parent of eb920a4 (Pirmkoda vienkāršošana)
 val_apm_veids = None
 vai_atr_pie_ielas = None
 velme_redz_nak_rezult = None
@@ -25,18 +28,57 @@ velme_redz_nak_rezult = None
 print("Programma, kas pēc lietotāja vajadzībām atrod izdevīgākās un pieejamākās bankas, kur apmainīt naudu no EUR uz USD nejuridiskām vajadzībām.\n")
 
 # val_apm_veids = vai lietotājs apmainīs valūtu skaidrā veidā vai caur bankas pārskaitījumu?
+<<<<<<< HEAD
 getAnswer(val_apm_veids,"Ievadiet 1, ja jūs apmainīsiet valūtu skaidrā veidā\nIevadiet 2, ja jūs apmainīsiet valūtu caur bankas pārskaitījumu\n")
+=======
+while True:
+   try: 
+      val_apm_veids = int(input("Ievadiet 1, ja jūs apmainīsiet valūtu skaidrā veidā\nIevadiet 2, ja jūs apmainīsiet valūtu caur bankas pārskaitījumu\n"))    
+      if val_apm_veids < 1:
+         raise Kludaj
+      elif val_apm_veids > 2:
+         raise Kluda
+      break
+   except:
+      print("Lūdzu ievadiet 1 vai 2.\n")
+>>>>>>> parent of eb920a4 (Pirmkoda vienkāršošana)
 
 # ja lietotājs apmaina val caur skaidru naudu un atrodas Rīgā, vai_atr_pie_ielas = vai lietotājs atrodas tuvumā pie Latvijas Bankas?
 if val_apm_veids == 1:
    atr_vieta = input("\nIevadiet jūsu atrašanās vietu\n")
    if atr_vieta.strip().lower() == "rīga" or atr_vieta.strip().lower() == "riga":
+<<<<<<< HEAD
       getAnswer(vai_atr_pie_ielas,"\nIevadiet 1, ja jūs atrodaties tuvumā pie K. Valdemāra ielas 2A. Ja neatrodaties, ievadiet 2.\n")
+=======
+      while True:
+         try:
+            vai_atr_pie_ielas = int(input("\nIevadiet 1, ja jūs atrodaties tuvumā pie K. Valdemāra ielas 2A. Ja neatrodaties, ievadiet 2.\n"))
+            if vai_atr_pie_ielas < 1:
+               raise Kluda
+            elif vai_atr_pie_ielas > 2:
+               raise Kluda
+            break
+         except:
+            print("Lūdzu ievadiet 1 vai 2.\n")
+>>>>>>> parent of eb920a4 (Pirmkoda vienkāršošana)
 
 # ja lietotājs atrodas tuvumā pie Latvijas Bankas, velme_redz_nak_rezult = vai lietotājs vēlas redzēt citas bankas, kur apmainīt naudu? 
 if vai_atr_pie_ielas == 1:
    print("\nVispieejamākā vieta, kur apmainīt valūtu ir Latvijas Banka.\n")
+<<<<<<< HEAD
    getAnswer(velme_redz_nak_rezult,"Ja jūs vēlaties redzēt citas bankas, kuras būtu izdevīgi apmainīt naudu, ievadiet 1. Ja nevēlaties, ievadiet 2.\n")
+=======
+   while True:
+      try:
+         velme_redz_nak_rezult = int(input("Ja jūs vēlaties redzēt citas bankas, kuras būtu izdevīgi apmainīt naudu, ievadiet 1. Ja nevēlaties, ievadiet 2.\n"))
+         if velme_redz_nak_rezult < 1:
+            raise Kluda
+         elif velme_redz_nak_rezult > 2:
+            raise Kluda
+         break
+      except:
+         print("Lūdzu ievadiet 1 vai 2.\n")
+>>>>>>> parent of eb920a4 (Pirmkoda vienkāršošana)
 else: # citādāk, ja lietotājs neatrodas tuvumā pie LB, tad ir vēlme redzēt citas bankas, kur apmainīt naudu
    velme_redz_nak_rezult = 1
 
@@ -47,4 +89,5 @@ if velme_redz_nak_rezult == 1:
    print("\n  Izdevīgāko banku tabula:")
    for Banka, Kurss in val_kursi_sorted:
       print("{:<21} {:<15}".format(Banka,Kurss))
-elif velme_redz_nak_rezult == 2: exit()
+elif velme_redz_nak_rezult == 2:
+   exit()
